dbdbe1c30dd4ed35ea8326e2c2181d6c25bb81f7
Marc Linster <marc@marclinster.com>
2026-02-05 11:01:38 +0100

    Updated dates

--- setup.sql
+++ setup.sql
@@ -1,7 +1,7 @@
 /*
 Create the sample databases that are needed for the DLH class 
 Introduction to SQL and Postgres
-
+Last updated 2026-01-12
 */
 
 -- dlh1 is an empty database that will be used to set up the muyuser, myproduct, myorder tables
